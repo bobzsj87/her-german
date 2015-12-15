fcbc1676e782df680f015b0e376c6f9d4409e577
Bob Zhou <bobzhou@Bobs-MacBook-Pro.local>
2015-12-15 15:33:26 +0100

    add json for testing

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
 var globalLang = "en-US";
 var ttsExtension = "fnolakpojfhnmkkmmofbicmehjodjekf";
-var recognizedSentence = "";
+var recognized = [];
+var flow;
+var currentFlow = 0;
+
+function showMessage(msg){
+  $(".message").html(msg);
+}
+
+function showResponse(msg){
+  $(".response").html(msg);
+}
+
+
 
 //cross extension
-function ttsSay(toSay, lang){
+function ttsSay(toSay, lang, cb){
+  showMessage(toSay);
   chrome.runtime.sendMessage(ttsExtension, {toSay:toSay, lang:lang},
     function(response) {
-      console.log(response);
-    }); 
+      cb(response);
+    });
+  
 }
-ttsSay("cross messaging success", globalLang);
 
 
 
 var recognition = new webkitSpeechRecognition();
 recognition.lang = globalLang;
-
-recognition.continuous = true;
+//recognition.continuous = true;
 
 recognition.onstart = function() {
   console.log("start recognition");
@@ -27,19 +39,48 @@ recognition.onerror = function(event) {
 };
 
 recognition.onend = function() {
-  console.log("end");
+  showResponse(recognized[recognized.length-1]);
+  triggerNext();
 };
 
 recognition.onresult = function(event) {
   var last = event.results.length - 1;
-  console.log(event.results[last][0].transcript);
+  recognized.push(event.results[last][0].transcript);
 };
 
-recognition.start();
 
+function triggerFlow(currentFlow){
+  if (currentFlow >= flow.length) {
+    //alert("end of the story");
+    return;
+  }
+  var f = flow[currentFlow];
 
-
-var first_char = /\S/;
-function capitalize(s) {
-  return s.replace(first_char, function(m) { return m.toUpperCase(); });
+  if (f.type == "say"){
+    ttsSay(f.content, globalLang, function(response){
+      triggerNext();
+    });
+  }
+  else if (f.type == "response"){
+    recognition.start();
+  }
 }
+
+function triggerNext(){
+  triggerFlow(++currentFlow);
+}
+
+$(document).ready(function(){
+  $.getJSON("test1.json", function(data){
+    flow = data.flow;
+    if (data.lang) globalLang = data.lang;
+
+    
+    triggerFlow(currentFlow);
+
+  }).fail(function() {
+    console.log("error");
+  })
+  //
+
+});
